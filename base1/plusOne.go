606f181e0bea8c9aefcf8412239d4799579c1dc7
lihao <3585964164@qq.com>
2025-10-07 18:10:43 +0800

    TK00005 加一

---
+++ base1/plusOne.go
@@ -0,0 +1,16 @@
+package base1
+
+func PlusOne(digits []int) []int {
+	for i := len(digits) - 1; i >= 0; i-- {
+		if digits[i] == 9 {
+			digits[i] = 0
+			continue
+		} else {
+			digits[i] = digits[i] + 1
+			return digits
+		}
+	}
+	newDigits := make([]int, len(digits)+1)
+	newDigits[0] = 1
+	return newDigits
+}
--- main.go
+++ main.go
@@ -4,12 +4,9 @@ import (
 	"fmt"
 
 	"github.com/holihaoa/task/base1"
-	_ "github.com/holihaoa/task/base1"
 )
 
 const mainName string = "main"
-
-var mainVar string = getMainVar()
 
 var nums [5]int = [5]int{1, 1, 3, 2, 2}
 
@@ -18,17 +15,15 @@ func init() {
 }
 
 func main() {
-	onceNumber := base1.GetOnceNumber(nums)
+	onceNumber := base1.GetOnceNumber(nums) // 第一题 只出现一次的数字
 	fmt.Println("getOnceNumber is ", onceNumber)
-	ispalindrome := base1.IsPalindrome(0)
+	ispalindrome := base1.IsPalindrome(0) // 第二题 回文数
 	fmt.Println("IsPalindrome is ", ispalindrome)
-	isvalide := base1.IsValid("(]")
+	isvalide := base1.IsValid("(]") // 第三题 有效括号
 	fmt.Println("IsValid is ", isvalide)
-	longestCommon := base1.LongestCommonPrefix([]string{"aaa", "aa", "aaa"})
+	longestCommon := base1.LongestCommonPrefix([]string{"aaa", "aa", "aaa"}) // 第四题 公共前缀
 	fmt.Println("LongestCommonPrefix is ", longestCommon)
-}
+	digits := base1.PlusOne([]int{0}) // 第四题 公共前缀
+	fmt.Println("PlusOne result is ", digits)
 
-func getMainVar() string {
-	fmt.Println("main.getMainVar method invoked!")
-	return mainName
 }
